193365e2699083fd01f582d32a88da0aa24436cf
Turxan20102 <160337949+Turxan20102@users.noreply.github.com>
2025-04-12 08:32:11 +0400

    Update tapsiriq1.py

--- tapsiriq1.py
+++ tapsiriq1.py
@@ -1,17 +1,17 @@
 a = float(input("Birinci ededi daxil et: "))
 b = float(input("İkinci ededi daxil et: "))
-emeliyyat = input("Əməliyyat sec (+, -, *, /): ")
+emeliyyat = input("Emeliyyat sec (+, -, *, /): ")
 
 if emeliyyat == "+":
-    print("Nəticə:", a + b)
+    print("Netice:", a + b)
 elif emeliyyat == "-":
-    print("Nəticə:", a - b)
+    print("Netice:", a - b)
 elif emeliyyat == "*":
-    print("Nəticə:", a * b)
+    print("Netice:", a * b)
 elif emeliyyat == "/":
     if b == 0:
         print("Sifira bölmek olmaz!")
     else:
-        print("Nəticə:", a / b)
+        print("Netice:", a / b)
 else:
-    print("Əməliyyat tanimadi!")
+    print("Emeliyyat tanimadi!")
